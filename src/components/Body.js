444ce9b0326848d1cf661ad552c42797b85b39c3
sagar <sagarc2580@gmail.com>
2024-03-11 18:59:16 +0530

    refactor: useeffect in Header component

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,9 @@ import Login from "./Login";
 import Browse from "./Browse";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Error from "./Error";
-import {auth} from '../utils/firebase'
-import {  onAuthStateChanged } from "firebase/auth";
-import {useDispatch} from "react-redux";
-import {addUser, removeUser} from "../utils/userSlice";
 
 function Body(props) {
-    //  for dispatching an action
-    const dispatch = useDispatch()
+
 
 
 
@@ -28,24 +23,6 @@ function Body(props) {
             element: <Error/>
         }
     ])
-
-    useEffect(() => {
-        // added event listner
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // if user is signed in (if my user is present)
-                const {uid , email , displayName, photoURL} = user;
-
-            //   dispatching add user action and it will store in redux store
-                dispatch(addUser({uid:uid , email:email , displayName:displayName , photoURL: photoURL}))
-            } else {
-               // if user is signed out
-               //   dispatching remove user action which automatically makes null in redux store state
-                dispatch(removeUser())
-            }
-        });
-
-    }, []);
 
 
     return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { FaSignOutAlt } from "react-icons/fa";
-import { signOut } from "firebase/auth";
+import {onAuthStateChanged, signOut} from "firebase/auth";
 import {auth} from '../utils/firebase'
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
+import {addUser, removeUser} from "../utils/userSlice";
+import {useDispatch} from "react-redux";
 function Header(props) {
     const navigate = useNavigate()
-
-
+//  for dispatching an action
+    const dispatch = useDispatch()
     const user = useSelector(store=>store.user)
 
     function handleSignOut(){
@@ -21,6 +23,34 @@ function Header(props) {
             navigate('*')
         });
     }
+
+    //  changed use effect here now header is in whole app also it is in router provider
+
+    useEffect(() => {
+        // added event listner
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // if user is signed in (if my user is present)
+                const {uid , email , displayName, photoURL} = user;
+
+                //   dispatching add user action and it will store in redux store
+                dispatch(addUser({uid:uid , email:email , displayName:displayName , photoURL: photoURL}))
+                navigate('/browse')
+
+            } else {
+                // if user is signed out
+                //   dispatching remove user action which automatically makes null in redux store state
+                dispatch(removeUser())
+
+                navigate('/')
+            }
+        });
+
+    }, []);
+
+
+
+
     return (
         <div className="absolute flex justify-between w-full px-8 py-2 bg-gradient-to-b from-black max-sm-[500px]:px-0 z-10 ">
             <img className="w-44 " src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
