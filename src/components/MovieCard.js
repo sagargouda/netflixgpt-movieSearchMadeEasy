193260b803350f0eb654c52ef1dd0a68fa07786a
sagar <sagarc2580@gmail.com>
2024-03-16 19:15:57 +0530

    styles: bg updated

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,12 +3,14 @@ import {IMG_CDN_URL} from "../constants/constants";
 
 function MovieCard({posterPath}) {
     // console.log(posterPath)
-    console.log(posterPath)
+    if(!posterPath){
+        return null
+    }
     return (
         <div className="w-48 pr-4">
-            {
-                posterPath !== null ? <img className="border-2" src={IMG_CDN_URL + posterPath} alt="Movie Card"/> : <h1 className="text-white mt-24">No Image available</h1>
-            }
+
+                 <img className="border-2" src={IMG_CDN_URL + posterPath} alt="Movie Card"/>
+
 
         </div>
     );
--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -12,14 +12,14 @@ function Suggestions(props) {
     }
 
     return (
-        <div className="p-4 m-4 bg-black text-white">
+        <div className="p-4 m-4 bg-black text-white bg-opacity-70">
             <div>
                 {
                     movieNames.map((movieName,index)=>{
                         return  <MovieList
                             key={movieName}
                             title={movieName}
-                            movie={movieResults[index]}/>
+                            movie={movieResults[index] !== 0 ? movieResults[index] : null}/>
                     })
                 }
             </div>
