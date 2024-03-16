0fccf46f2fb205aeab584c0a0ea0e415f394e1b7
sagar <sagarc2580@gmail.com>
2024-03-16 18:17:45 +0530

    feat: add reducers for movie search and movie result

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import lang from "../utils/languagesConstants";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useRef} from 'react'
 import openai from "../utils/openAi";
+import {API_OPTIONS} from "../constants/constants";
+import {addGptMovieResult} from "../utils/gptSlice";
+
 
 function GptSearchBar(props) {
     const language = useSelector(store => store.config)
 
+    //  dispatching gpt movies in store
+    const dispatch = useDispatch()
+
+
     // use ref for getting the text value
     const searchText = useRef(null)
+
+  //   movie search in tmdb
+    const searchMovieTMDB = async (movie) =>{
+        const data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`,API_OPTIONS)
+      const json = await data.json()
+
+        return json.results
+    }
+
+
+
+
   async function handleGptSearchClick(){
     console.log(searchText.current.value)
 
@@ -26,10 +45,20 @@ function GptSearchBar(props) {
          // console.log(gptResults.choices[0]?.message?.content)
 
     const gptMovies = gptResults.choices[0]?.message?.content.split(',')
-      // console.log(gptMovies)
+     // console.log(gptResults.choices[0]?.message?.content)
 
+    //   for each movie in gptMovies i will search in tmdb api
 
+const promiseArray  = gptMovies.map((searchMovie)=>{
+    // array of promises which are in pending state (5 network calls made)
+    return searchMovieTMDB(searchMovie)
+})
 
+      // promise all for resolving all my promises in promises array
+      const tmdbResults = await Promise.all(promiseArray)
+
+  // console.log(tmdbResults)
+      dispatch(addGptMovieResult({ movieNames:gptMovies,movieResults:tmdbResults }))
     };
 
 
