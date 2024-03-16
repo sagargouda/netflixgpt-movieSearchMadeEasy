92dff8a6338b1527c70c105176be90a59cf485bc
sagar <sagarc2580@gmail.com>
2024-03-16 18:53:18 +0530

    feat: reused movieList component for dynamically rendering cards

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import MovieList from "./MovieList";
 
 function Suggestions(props) {
+
+    const gpt = useSelector(store => store.gpt);
+    const { movieNames , movieResults } = gpt;
+
+    if(!movieNames ){
+        return null
+    }
+
     return (
-        <div></div>
+        <div className="p-4 m-4 bg-black text-white">
+            <div>
+                {
+                    movieNames.map((movieName,index)=>{
+                        return  <MovieList
+                            key={movieName}
+                            title={movieName}
+                            movie={movieResults[index]}/>
+                    })
+                }
+            </div>
+        </div>
     );
 }
 
