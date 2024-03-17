c098f42659a9024bc2cf99236561316faf9f5ee2
sagar <sagarc2580@gmail.com>
2024-03-17 18:18:54 +0530

    refactor: changed

--- src/utils/openAi.js
+++ src/utils/openAi.js
@@ -4,7 +4,6 @@ import {OPENAI_KEY} from "../constants/constants";
 //  this gives me helper functions
 const openai = new OpenAI({
     apiKey: OPENAI_KEY, // This is the default and can be omitted
-  dangerouslyAllowBrowser: true
 });
 
 export default openai
