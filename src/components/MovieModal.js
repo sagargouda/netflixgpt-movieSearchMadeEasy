e1364a8e5df0ab0fc2e801c83773ea758282a368
sagar <sagarc2580@gmail.com>
2024-03-17 10:36:03 +0530

    feat: add movie name

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -13,9 +13,9 @@ function MovieModal({ isOpen  , movieModal}) {
                 <div className={`inline-block align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full ${isOpen ? 'sm:scale-100' : 'sm:scale-0'}`} role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <div className="sm:flex sm:items-start">
-                            <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                            <div className="mt-3  text-center sm:mt-0 sm:text-left w-full">
                                 <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                                    {movieModal?.original_title}
+                                  <span>Movie Name =></span> {movieModal?.original_title}
                                 </h3>
                                 <div className="mt-2">
                                     <p className="text-sm text-gray-500">
